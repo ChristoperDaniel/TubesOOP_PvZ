07c98ddfac36d089647be8dcc94f9a09ea5855af
kayla1410 <130771373+kayla1410@users.noreply.github.com>
2024-05-07 13:35:10 +0700

    aquatic

--- Aquatic.java
+++ Aquatic.java
@@ -1,1 +1,11 @@
-public 
+public abstract class Aquatic {
+    private boolean is_aquatic;
+
+    public Aquatic(boolean is_aquatic){
+        this.is_aquatic = is_aquatic;
+    }
+
+    public boolean getAquatic(){
+        return is_aquatic;
+    }
+}
