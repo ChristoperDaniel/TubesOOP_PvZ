1c528df761e1e327981d9d52695a0b66be495ec0
ChristoperDaniel <christoper.daniel04@gmail.com>
2024-05-15 20:39:59 +0700

    commit

--- classes/map/Map.java
+++ classes/map/Map.java
@@ -1,5 +1,12 @@
 package classes.map;
+
+
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+
+import Tanaman.Tanaman;
+import Zombie.Zombie;
 
 public class Map {
     private class Tile {
@@ -28,10 +35,17 @@ public class Map {
             }
         }
 
-        public void addPlant(String plantSymbol) {
+        public void addPlantTile(String plantSymbol) {
             if (isTileAvail) {
                 this.symbol = plantSymbol;
                 this.isTileAvail = false;
+            }
+        }
+
+        public void removePlant() {
+            if (!isTileAvail && !isAreaSpawn) {
+                this.symbol = "_";
+                this.isTileAvail = true;
             }
         }
 
@@ -45,6 +59,9 @@ public class Map {
     public static int columns = 11;
     private Tile[][] map;
 
+    private ConcurrentHashMap<Integer, List<Tanaman>> tanamanLanes;
+    private ConcurrentHashMap<Integer, List<Zombie>> zombieLanes;
+
     public Map() {
         map = new Tile[rows][columns];
         initializeMap();
@@ -54,13 +71,17 @@ public class Map {
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < columns; col++) {
                 boolean isAreaSpawn = (col == 10);
-                boolean isAreaRumput = ((row == 0 || row == 1 || row == 4 || row == 5) && (col >= 1 && col <= 9));
+                boolean isAreaRumput = ((row == 0 || row == 1 || row == 4 || row == 5) && (col >= 2 && col <= 10));
                 boolean isAreaRumah = (col == 0);
                 boolean isTileAvail = true;
 
                 map[row][col] = new Tile(isAreaSpawn, isAreaRumput, isAreaRumah, isTileAvail);
             }
         }
+    }
+
+    public Tile getTiles(int row, int column){
+        return map[row][column];
     }
 
     public void spawnZombie() {
@@ -74,7 +95,13 @@ public class Map {
 
     public void addPlant(int row, int col, String plantSymbol) {
         if (row >= 0 && row < rows && col >= 0 && col < columns) {
-            map[row][col].addPlant(plantSymbol);
+            map[row][col].addPlantTile(plantSymbol);
+        }
+    }
+
+    public void removePlant(int row, int col) {
+        if (row >= 0 && row < rows && col >= 0 && col < columns) {
+            map[row][col].removePlant();
         }
     }
 
@@ -94,6 +121,10 @@ public class Map {
         gameMap.spawnZombie();
         gameMap.addPlant(2, 5, "P"); // Adding Peashooter at (2, 5)
         gameMap.addPlant(4, 3, "S"); // Adding SunFlower at (4, 3)
+        gameMap.displayMap();
+        gameMap.removePlant(2, 5); // Removing Peashooter from (2, 5)
+        gameMap.removePlant(4, 3); // Removing SunFlower from (4, 3)
+        System.out.println("After removing plants:");
         gameMap.displayMap();
     }
 }
