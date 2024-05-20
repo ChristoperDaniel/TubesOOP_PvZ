211c9e3dd3ae48239a3e33c61925303abd693b75
lkhairul <lkhairulamal651@gmail.com>
2024-05-21 00:53:15 +0700

    test

--- plant/TanamanPenyerang.java
+++ plant/TanamanPenyerang.java
@@ -1,17 +1,4 @@
 package plant;
-import classes.map.*;
-import zombie.DolphinRiderZombie;
-import zombie.JackInTheBoxZombie;
-import zombie.PoleVaultingZombie;
-import zombie.Zombie;
-import classes.objects.*;
-import interfaces.ZombieWithAbility;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 /**
  * InnerTanamanPenyerang
