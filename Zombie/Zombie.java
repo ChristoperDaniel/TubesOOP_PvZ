300b7ff0623c613e5419586fd3c67c8b039a516f
lkhairul <lkhairulamal651@gmail.com>
2024-05-23 18:14:32 +0700

    Thread zombie

--- Zombie/Zombie.java
+++ Zombie/Zombie.java
@@ -2,15 +2,9 @@ package zombie;
 
 import classes.map.*;
 import plant.*;
-
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 
 public abstract class Zombie extends Aquatic {
-    private ScheduledExecutorService executorService;
     private String namaZombieString;
     private int healthZombie;
     private int attack_damageZombie;
@@ -35,7 +29,6 @@ public abstract class Zombie extends Aquatic {
         this.is_get_slowedZombie = false;
         this.row = -1;
         this.col = -1;
-        executorService = Executors.newSingleThreadScheduledExecutor();
     }
 
     public String getNamaZombie() {
@@ -114,48 +107,34 @@ public abstract class Zombie extends Aquatic {
         this.is_ability_used = is_ability_used;
     }
 
-    public void slowedZombie(boolean is_get_slowedZombie) {
-        if (is_get_slowedZombie) {
-            setSpeedZombie(getSpeedZombie() + 5000);
-            setAttackSpeedZombie(getAttackSpeedZombie() + 500);
-        }
-    }
-    
     public void attackZombie(Tile tile, Map map, Tanaman tanaman) {
         int x = getRowZombie();
         int y = getColZombie();
         Tile xy = map.getTile(x, y);
         int dmg = getAttackDamageZombie();
-        int atkspd = getAttackSpeedZombie();
         List<Tanaman> planted = xy.getTanaman();
         if (getRangeZombie() == 1) {
-            executorService.scheduleAtFixedRate(() -> {
-                for (Tanaman t : planted) {
-                    t.setHealthTanaman(t.getHealthTanaman() - dmg);
-                    if (t.getHealthTanaman() <= 0) {
-                        xy.removeTanaman(t);
-                        executorService.shutdown();
-                    }
+            for (Tanaman t : planted) {
+                t.setHealthTanaman(t.getHealthTanaman() - dmg);
+                if (t.getHealthTanaman() <= 0) {
+                    xy.removeTanaman(t);
                 }
-            } , 0, atkspd, TimeUnit.MILLISECONDS);
+            }
         }
 
         else {
-            executorService.scheduleAtFixedRate(() -> {
-                for (int i = y; i > 0; i--) {
-                    Tile xy1 = map.getTile(x, i);
-                    List<Tanaman> planted1 = xy1.getTanaman();
-                    if (planted1 != null) {
-                        for (Tanaman t : planted1) {
-                            t.setHealthTanaman(t.getHealthTanaman() - dmg);
-                            if (t.getHealthTanaman() <= 0) {
-                                xy1.removeTanaman(t);
-                                executorService.shutdown();
-                            }
+            for (int i = y; i > 0; i--) {
+                Tile xy1 = map.getTile(x, i);
+                List<Tanaman> planted1 = xy1.getTanaman();
+                if (planted1 != null) {
+                    for (Tanaman t : planted1) {
+                        t.setHealthTanaman(t.getHealthTanaman() - dmg);
+                        if (t.getHealthTanaman() <= 0) {
+                            xy1.removeTanaman(t);
                         }
                     }
                 }
-            }, 0, atkspd, TimeUnit.MILLISECONDS);
+            }
         }
     }
 
