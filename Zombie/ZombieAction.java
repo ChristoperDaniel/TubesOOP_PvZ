fb2969c2c94d07358e323abc8d9dc8a8fdef8aff
lkhairul <lkhairulamal651@gmail.com>
2024-05-24 18:21:16 +0700

    Revisi

--- Zombie/ZombieAction.java
+++ Zombie/ZombieAction.java
@@ -24,7 +24,7 @@ public class ZombieAction implements Runnable {
     @Override
     public void run() {
         // Lakukan aksi zombie dalam loop
-        while (zombie.getHealthZombie() > 0) {
+        while (zombie.getHealthZombie() > 0 && zombie.getColZombie() > 0) {
             // Sinkronisasi untuk menghindari akses bersamaan ke tile
                 // Cek apakah ada tanaman di tile yang sama dengan posisi zombie
                 List<ZombieWithAbility> zability = new ArrayList<>();
@@ -36,40 +36,41 @@ public class ZombieAction implements Runnable {
                 int y = zombie.getColZombie();
                 Tile tile1 = map.getTile(x, y);
                 List<Tanaman> tanaman = tile1.getTanaman();
-                
-                if ((!tanaman.isEmpty()) && zombie.getIsAbilityUsed()) {
-                    // Jika ada tanaman, serang tanaman
-                    for (ZombieWithAbility zombie : zability) {
-                        zombie.useAbility(is_Ability_Used, tile, map, tanaman.get(tanaman.size()-1));
+                for (Tanaman t : tanaman) {
+                    if ((!tanaman.isEmpty()) && zombie.getIsAbilityUsed()) {
+                        // Jika ada tanaman, serang tanaman
+                        for (ZombieWithAbility zombie : zability) {
+                            zombie.useAbility(is_Ability_Used, tile, map, tanaman.get(tanaman.size()-1));
+                        }
+                    }
+
+                    else if ((!tanaman.isEmpty()) && (!zombie.getIsAbilityUsed()) && (!zombie.getIsGetSlowedZombie())) {
+                        // Jika ada tanaman, serang tanaman
+                        try {
+                            System.out.println("test..............................");
+                            Thread.sleep(zombie.getAttackSpeedZombie()); // Misalnya menunggu 1 detik antara setiap aksi
+                            zombie.attackZombie(tile, map, t);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    } 
+
+                    else if ((!tanaman.isEmpty()) && (!zombie.getIsAbilityUsed()) && zombie.getIsGetSlowedZombie()){
+                        // Jika tidak ada tanaman, bergerak
+                        try {
+                            Thread.sleep(zombie.getAttackSpeedZombie() + 500); // Misalnya menunggu 1 detik antara setiap aksi
+                            zombie.attackZombie(tile, map, t);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
                     }
                 }
 
-                else if ((!tanaman.isEmpty()) && (!zombie.getIsAbilityUsed()) && (!zombie.getIsGetSlowedZombie())) {
-                    // Jika ada tanaman, serang tanaman
-                    try {
-                        System.out.println("test..............................");
-                        Thread.sleep(zombie.getAttackSpeedZombie()); // Misalnya menunggu 1 detik antara setiap aksi
-                        zombie.attackZombie(tile, map);
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                    }
-                } 
-
-                else if (tanaman.isEmpty() && (!zombie.getIsAbilityUsed()) && (!zombie.getIsGetSlowedZombie())){
+                if (tanaman.isEmpty() && (!zombie.getIsAbilityUsed()) && (!zombie.getIsGetSlowedZombie())){
                     // Jika tidak ada tanaman, bergerak
                     try {
                         Thread.sleep(zombie.getSpeedZombie()); // Misalnya menunggu 1 detik antara setiap aksi
                         zombie.moveZombie(map);
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                    }
-                }
-
-                else if ((!tanaman.isEmpty()) && (!zombie.getIsAbilityUsed()) && zombie.getIsGetSlowedZombie()){
-                    // Jika tidak ada tanaman, bergerak
-                    try {
-                        Thread.sleep(zombie.getAttackSpeedZombie() + 500); // Misalnya menunggu 1 detik antara setiap aksi
-                        zombie.attackZombie(tile, map);
                     } catch (InterruptedException e) {
                         Thread.currentThread().interrupt();
                     }
